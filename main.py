fdeb8724a07b5d68a4241b1d4cef281bad641c2d
Diesel127 <retina127127@gmail.com>
2025-12-11 16:42:03 +0100

    feature1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("first commit")
+def get_evens_positive(n: int) -> list:
+    result = list(filter(lambda x: x % 2 == 0, range(0, n + 1)))
+    return result
+
+def get_evens_negative(n: int) -> list:
+    result = [i for i in range(n, 0) if i % 2 == 0]
+    return result
+
+def main() -> None:
+    while True:
+        try:
+            user_input = int(input("Enter integer: "))
+        except Exception as e:
+            print(f"Error occured: {e}, please enter integer(example: 10, -10")
+        else:
+            if user_input == 0:
+                print("0")
+            elif user_input > 0:
+                print(get_evens_positive(user_input))
+            else:
+                print(get_evens_negative(user_input))
+if __name__ == "__main__":
+    main()
